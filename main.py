df32cae797e702d2f2d4ebffb39e6aeb9a64f140
offbrandd <btytler411@gmail.com>
2023-03-28 15:23:25 -0700

    initial commit
    currently:
    opens an automated chromium instance via selenium
    traverses through a csv
    inputs a list of serial numbers into Dell's website
    scrapes the page for warranty status and expiration

---
+++ main.py
@@ -0,0 +1,96 @@
+from time import sleep
+from datetime import datetime
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import csv
+
+# Return a reader object which will
+# iterate over lines in the given csvfile
+csv_reader = csv.reader(open('data.csv', 'r'))
+
+# convert string to list
+list_of_csv = list(csv_reader)
+
+
+def setup_chrome():
+    site = 'https://www.dell.com/support/home/en-us'
+    # set the driver
+    driver = webdriver.Chrome()
+
+    # load this site
+    driver.get(site)
+    return driver
+
+
+def checkDell(driver, sn):  # TODO return boolean status and date
+    sleep(1)
+    # wait until search box loaded in, select it, send serial number, press enter
+    element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+        (By.ID, "mh-search-input")))
+    # element.send_keys(sn)
+    # sleep(0.8)
+    entered = False
+    while not entered:
+        try:
+            entered = True
+            element.clear()
+            element.send_keys(sn)
+            element.send_keys(Keys.ENTER)
+
+        except selenium.common.exceptions.StaleElementReferenceException:
+            entered = False
+    try:
+        # wait until warranty check text ready
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+            (By.XPATH, "//*[@class='warrantyExpiringLabel mb-0 ml-1 mr-1']")))
+    except:
+        return False, False
+    # checks text and returns warranty status and bool representing whether or not check was successful
+    if "Expired" in element.text:
+        return False, True
+    elif "Expires" in element.text:
+        return True, True
+
+
+# make runable
+if __name__ == '__main__':
+    driver = setup_chrome()
+
+    total_check = 0  # TEMP check how many assets we've checked
+    start_time = datetime.now()
+
+    # iterate through csv, calling checkDell
+    i = 1
+    # selenium will occaisionally outpace itself and input a second serial number into search box
+    # catch this by checking for search failure. If search fails, interator does not increase and loop continues at same line
+    # fail_count tracks failures per line. if search failures multiple times, improper serial number data likely present
+    fail_count = 0
+    while i < len(list_of_csv):
+        in_warranty, check_successful = checkDell(driver, list_of_csv[i][8])
+        if check_successful:  # yay, save warranty status and date TODO: save warranty status and date
+            print(str(i) + " " + str(in_warranty))
+            i += 1
+            fail_count = 0
+            total_check += 1
+        elif fail_count < 3:  # notate failure, try again
+            fail_count += 1
+        else:  # too many failures, skip entry
+            fail_count = 0
+            i += 1
+            total_check += 1
+
+    end_time = datetime.now()
+
+    print("# of assets: " + total_check + "  start: " +
+          start_time + "  end: " + end_time)
+
+    # and chill a bit
+    sleep(7)
+    # k, cool. let's bounce.
+    driver.quit()
