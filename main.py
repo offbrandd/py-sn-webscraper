1ae22e10741ac106808e0997fcbf9e1d03b40aae
offbrandd <btytler411@gmail.com>
2023-03-30 07:14:42 -0700

    refactored a bit
    no functionality changes
    still haven't gotten a way of keeping script from running over itself

--- main.py
+++ main.py
@@ -10,26 +10,25 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import csv
 
-# Return a reader object which will
-# iterate over lines in the given csvfile
 csv_reader = csv.reader(open('data.csv', 'r'))
-
 # convert string to list
 list_of_csv = list(csv_reader)
+
+searching = False
 
 
 def setup_chrome():
     site = 'https://www.dell.com/support/home/en-us'
     # set the driver
     driver = webdriver.Chrome()
-
     # load this site
     driver.get(site)
     return driver
 
 
 def checkDell(driver, sn):  # TODO return boolean status and date
-    sleep(1)
+    sleep(0.4)
+    searching = True
     # wait until search box loaded in, select it, send serial number, press enter
     element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
         (By.ID, "mh-search-input")))
@@ -55,9 +54,15 @@ def checkDell(driver, sn):  # TODO return boolean status and date
 
     # checks text and returns warranty status and bool representing whether or not check was successful
     if "Expired" in element_text:
+        searching = False
         return False, True
-    elif "Expires" in element_text:  # TODO include prosupport
+    elif "Expires" in element_text or "ProSupport" in element_text:
+        searching = False
         return True, True
+    else:
+        print("NO WARNRANTY FOUND??")
+        searching = False
+        return False, False
 
 
 # make runable
@@ -69,23 +74,15 @@ if __name__ == '__main__':
 
     # iterate through csv, calling checkDell
     i = 1
-    # selenium will occaisionally outpace itself and input a second serial number into search box
-    # catch this by checking for search failure. If search fails, interator does not increase and loop continues at same line
-    # fail_count tracks failures per line. if search failures multiple times, improper serial number data likely present
-    fail_count = 0
+
     while i < len(list_of_csv):
-        in_warranty, check_successful = checkDell(driver, list_of_csv[i][8])
-        if check_successful:  # yay, save warranty status and date TODO: save warranty status and date
-            print(str(i) + " " + str(in_warranty))
-            i += 1
-            fail_count = 0
-            total_check += 1
-        elif fail_count < 3:  # notate failure, try again
-            fail_count += 1
-        else:  # too many failures, skip entry
-            fail_count = 0
-            i += 1
-            total_check += 1
+        if not searching:
+            in_warranty, check_successful = checkDell(
+                driver, list_of_csv[i][8])
+            if check_successful:  # yay, save warranty status and date TODO: save warranty status and date
+                print(str(i) + " " + str(in_warranty))
+                i += 1
+                total_check += 1
 
     end_time = datetime.now()
 
