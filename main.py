9eccb68f6115967ab876141284c569cb2503e5b4
offbrandd <btytler411@gmail.com>
2023-05-30 13:31:38 -0700

    redo
    no more thread.sleep, ONLY waits for elements
    very slow, will try multithreading
    again, data excluded for privacy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from datetime import datetime
+import os
 
 import selenium
 from selenium import webdriver
@@ -8,9 +9,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 import csv
 
-csv_reader = csv.reader(open('data.csv', 'r'))
+csv_reader = csv.reader(open("data.csv", "r"))
 # convert string to list
 list_of_csv = list(csv_reader)
 
@@ -18,74 +20,83 @@ searching = False
 
 
 def setup_chrome():
-    site = 'https://www.dell.com/support/home/en-us'
+    site = "https://www.dell.com/support/home/en-us"
     # set the driver
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=chrome_options)
     # load this site
     driver.get(site)
     return driver
 
 
-def checkDell(driver, sn):  # TODO return boolean status and date
-    sleep(0.4)
-    searching = True
-    # wait until search box loaded in, select it, send serial number, press enter
-    element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
-        (By.ID, "mh-search-input")))
-
-    entered = False
-    while not entered:
-        try:
-            entered = True
-            element.clear()
-            element.send_keys(sn)
-            element.send_keys(Keys.ENTER)
-
-        except:
-            entered = False
+def checkDell(
+    driver, sn
+):  # searching s/n argument on dell page, returns expiration date text
+    wait = WebDriverWait(driver, 10)
+    try:
+        element = wait.until(
+            EC.presence_of_element_located((By.ID, "mh-search-input"))
+        )  # wait until element is present in page (not necessarily visible)
+        element.clear()
+        element.send_keys(sn)
+    except:
+        return "COULD NOT FIND SEARCH BAR"
 
     try:
-        # wait until warranty check text ready
-        element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
-            (By.XPATH, "//*[@class='warrantyExpiringLabel mb-0 ml-1 mr-1']")))
-        element_text = element.text
+        wait.until(
+            EC.text_to_be_present_in_element_value((By.ID, "mh-search-input"), sn)
+        )
+        element.send_keys(Keys.ENTER)
     except:
-        return False, False
+        return "COULD NOT SEARCH"
 
-    # checks text and returns warranty status and bool representing whether or not check was successful
-    if "Expired" in element_text:
-        searching = False
-        return False, True
-    elif "Expires" in element_text or "ProSupport" in element_text:
-        searching = False
-        return True, True
-    else:
-        print("NO WARNRANTY FOUND??")
-        searching = False
-        return False, False
+    try:
+        element = wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "//*[@class='warrantyExpiringLabel mb-0 ml-1 mr-1']",
+                )
+            )
+        )
+        return element.text
+    except:
+        return "SN LOOKUP FAILURE"
+
+
+def updateProgress(progress, total):
+    os.system("clear")
+    print(str(progress) + "/" + str(total) + "\n")
+    percentage = progress / total
+    bar_length = 50
+    p = round(percentage * bar_length)
+    s = "["
+    for i in range(p):
+        s += "#"
+    for i in range(bar_length - p):
+        s += "-"
+    s += "]"
+    print(s)
 
 
 # make runable
-if __name__ == '__main__':
+if __name__ == "__main__":
     driver = setup_chrome()
-
-    total_check = 0  # TEMP check how many assets we've checked
-    start_time = datetime.now()
+    length = len(list_of_csv)
 
     # iterate through csv, calling checkDell
     i = 1
-
     while i < len(list_of_csv):
-        if not searching:
-            in_warranty, check_successful = checkDell(
-                driver, list_of_csv[i][8])
-            if check_successful:  # yay, save warranty status and date TODO: save warranty status and date
-                print(str(i) + " " + str(in_warranty))
-                i += 1
-                total_check += 1
+        updateProgress(i, length)
+        list_of_csv[i][6] = checkDell(driver, list_of_csv[i][2])
+        i += 1
+    file = open("output.csv", "w+", newline="")
 
-    end_time = datetime.now()
-
-    print("# of assets: " + str(total_check) + "  start: " +
-          str(start_time) + "  end: " + str(end_time))
+    with file:
+        write = csv.writer(file)
+        write.writerows(list_of_csv)
+    print(list_of_csv)
     driver.quit()
